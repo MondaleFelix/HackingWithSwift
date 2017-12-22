0872c19898efe5d32dd0e011937e97aa604a0bc2
Mondale Felix <mondalefelix@yahoo.com>
2017-12-22 15:25:22 -0800

    added fileManager and path

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // allows us to work with the filesystem which we will use to look for files
+        let fm = FileManager.default
+        // path of our apps bundle
+        let path = Bundle.main.resourcePath!
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
